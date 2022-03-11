eae960fd431c6a89195b25df0aada91f538ed311
kauanbertalha <100381952+Berkhz@users.noreply.github.com>
2022-03-11 15:24:47 -0300

    Alura Fork

--- aula5/script.js
+++ aula5/script.js
@@ -2,12 +2,17 @@ var filmesFavoritosAlura = "adicionarFilme";
 function adicionarFilme() {
   var filmeFavorito = document.getElementById("filme").value;
   if (filmeFavorito.endsWith(".jpg")) {
-    var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
-    var elementoListaFilmes = document.getElementById("listaFilmes");
-    elementoListaFilmes.innerHTML =
-      elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+    listarFilmesNaTela(filmeFavorito);
   } else {
     console.error("Endereço de filme inválido");
   }
   document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela(filmeFavorito) {
+  console.log("filmeFavorito");
+  var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
 }
