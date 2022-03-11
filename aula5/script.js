52cadcb94cbae9d800a76055ce40603c4da1ef94
kauanbertalha <100381952+Berkhz@users.noreply.github.com>
2022-03-11 14:33:31 -0300

    script

---
+++ aula5/script.js
@@ -0,0 +1,5 @@
+function adicionarFilme() {
+    var campoFilmeFavorito = document.getElementById("filme").ariaValueMax;
+    var elemntoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">"
+    console.log(campoFilmeFavorito);
+}
