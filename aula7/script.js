47142c05c10cdcc147a6935970a04076b97d22ca
kauanbertalha <100381952+Berkhz@users.noreply.github.com>
2022-03-16 14:28:30 -0300

    Final script

--- aula7/script.js
+++ aula7/script.js
@@ -43,54 +43,64 @@ var carta5 = {
   }
 };
 
-var cartas = [carta1, carta2, carta3, carta4, carta5];
 var cartaMaquina;
 var cartaJogador;
+var cartas = [carta1, carta2, carta3, carta4, carta5];
+// 0                    1       2       3       4
 
 function sortearCarta() {
   var numeroCartaMaquina = parseInt(Math.random() * 5);
   cartaMaquina = cartas[numeroCartaMaquina];
-}
-  
+
   var numeroCartaJogador = parseInt(Math.random() * 5);
-  while (numeroCartaMaquina == numeroCartaJogador) {
-    numeroCartaJogador = parseInt(Math.random() * 3);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * 5);
+  }
   cartaJogador = cartas[numeroCartaJogador];
   console.log(cartaJogador);
-  }
 
   document.getElementById("btnSortear").disabled = true;
   document.getElementById("btnJogar").disabled = false;
-
   exibirOpcoes();
-
+}
 
 function exibirOpcoes() {
   var opcoes = document.getElementById("opcoes");
-  var opcoesTexto = ""
-  
+  var opcoesTexto = "";
   for (var atributo in cartaJogador.atributos) {
-    opcoesTexto += 
-      "<input type='radio' name='atributo' 
-    value='" + 
-      atributo + 
-      "'>"" +
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
       atributo;
   }
   opcoes.innerHTML = opcoesTexto;
 }
 
-
 function obtemAtributoSelecionado() {
-  var radioAtributos = document.getElementsByName("atributo");
-  
-  for (var i = 0; i < radioAtributos.lenght; i++) {
-    if (radioAtributos[i].checked) {
-      
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
     }
   }
 }
 
 function jogar() {
-  var atributoSelecionado = obtemAtribudoSelecionado()
+  var atributoSelecionado = obtemAtributoSelecionado();
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    alert("Venceu a carta máquina");
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    alert("Perdeu. Carta da máquina é maior");
+  } else {
+    alert("Empatou!");
+  }
+  console.log(cartaMaquina);
 }
